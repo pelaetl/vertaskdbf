287416636097ed9285d81902cd680bbab15bed7f
pelaetl <pedroll2109@gmail.com>
2025-07-24 21:57:55 -0300

    Atualização RepositoryConfig

--- src/main/java/br/cefetmg/vertask/config/RepositoryConfig.java
+++ src/main/java/br/cefetmg/vertask/config/RepositoryConfig.java
@@ -2,6 +2,9 @@ package br.cefetmg.vertask.config;
 
 import br.cefetmg.vertask.repository.FuncionarioRepository;
 import br.cefetmg.vertask.repository.SetorRepository;
+import br.cefetmg.vertask.repository.TarefaRepository;
+import br.cefetmg.vertask.repository.UsuarioRepository;
+import br.cefetmg.vertask.repository.AdministradorRepository;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,4 +21,21 @@ public class RepositoryConfig {
     public FuncionarioRepository funcionarioRepository(Jdbi jdbi) {
         return jdbi.onDemand(FuncionarioRepository.class);
     }
+
+    @Bean
+    public TarefaRepository tarefaRepository(Jdbi jdbi) {
+        return jdbi.onDemand(TarefaRepository.class);
+    }
+
+    @Bean
+    public UsuarioRepository usuarioRepository(Jdbi jdbi) {
+        return jdbi.onDemand(UsuarioRepository.class);
+    }
+
+
+    @Bean
+    public AdministradorRepository administradorRepository(Jdbi jdbi) {
+        return jdbi.onDemand(AdministradorRepository.class);
+    }
+
 }
